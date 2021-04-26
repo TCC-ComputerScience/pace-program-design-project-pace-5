174775ed710dd6d06273315bbb7de5a1c298bd04
Christopher <43659794+Niwaessence@users.noreply.github.com>
2021-04-25 21:53:13 -0400

    pseudocode

---
+++ PACE Github Assignment -- Part 2 (Initial Plan).cpp
@@ -0,0 +1,35 @@
+//Christopher Backus
+//CSC201
+// 4/25/2021
+// Board game of battleship
+#include <iostream>
+
+using namespace std;
+
+//pre takes array to generate the board
+//Places symbols to fill the array
+//post sends out map
+void GenerateMap()
+
+//pre take numbers from the user
+// checks the numbers in the array to see if it hit
+//post nothing
+void CheckHit(int x, int y)
+
+int main(){
+
+    //create enum for symbols
+
+    //Make starting array for the board
+
+    //Then generate ship places
+
+   //While loop
+
+   //Allow the user to guess numbers to see if they get a hit on the ships
+
+   //Keeps going until all ships are gone.
+
+
+    return 0;
+}
